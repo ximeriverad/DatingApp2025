197d5b9fbcabd01288dffea480b3db65369f937b
ximeriverad <al261261@edu.uaa.mx>
2025-08-30 22:09:00 -0600

    Parcial01 - Ad First EF Migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 
 var app = builder.Build();
 
