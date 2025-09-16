abef6c56484c4b0b4ca4664fe3ef04a73fd0ecd9
ximeriverad <al261261@edu.uaa.mx>
2025-09-16 13:41:56 -0600

    Parcial 02 - Add Password hash salt

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,6 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString(); //el uso de NewGuid es por seguridad y para no permitir nulls
     public required string DisplayName { get; set; } //? acepta nulos y opcionales
     public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
