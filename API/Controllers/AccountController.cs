491f0972c1eeb63b01b02a95f4cc3035b856af9f
ximeriverad <al261261@edu.uaa.mx>
2025-09-20 19:14:01 -0600

    Parcial02 - Add AppUser extension method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,15 +28,10 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         };
 
         context.Users.Add(user);
+        
         await context.SaveChangesAsync();
 
-        return new UserResponse
-        {
-            Id = user.Id,
-            DisplayName = user.DisplayName,
-            Email = user.Email,
-            Token = tokenService.CreateToken(user)
-        };
+        return user.ToDto(tokenService);
     }
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> Login(LoginRequest request)
@@ -53,13 +49,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
             if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid email or password");
         }
 
-        return new UserResponse
-        {
-            Id = user.Id,
-            DisplayName = user.DisplayName,
-            Email = user.Email,
-            Token = tokenService.CreateToken(user)
-        };
+        return user.ToDto(tokenService);
     } 
 
     private async Task<bool> EmailExists(string email)
