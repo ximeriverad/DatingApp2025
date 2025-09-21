a01ea0812e8abb521117f2bec47a21b43d2272f7
ximeriverad <al261261@edu.uaa.mx>
2025-09-20 18:43:12 -0600

    Parcial02 - Add JWT Middleware

--- API/Controllers/MembersControllers.cs
+++ API/Controllers/MembersControllers.cs
@@ -1,25 +1,28 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+[Authorize]
 public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
-    public async Task < ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
         var members = await context.Users.ToListAsync();
         return members;
     }
+    [AllowAnonymous]
     [HttpGet("{id}")]//htttps://localhost:5001/api/members/bob-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
         var member = await context.Users.FindAsync(id);
 
         if (member == null) return NotFound();
-        
+
         return member;
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,18 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+     var tokenKey = builder.Configuration["TokenKey"]
+        ?? throw new ArgumentNullException("Cannot get the token key - Program.cs");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+});
 
 var app = builder.Build();
 
@@ -27,7 +42,3 @@ app.UseCors(x => x.AllowAnyHeader()
 app.MapControllers();
 
 app.Run();
-
-internal interface IUserRepository
-{
-}
