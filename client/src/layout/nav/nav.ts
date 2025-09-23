999529e213e3e1d06b73f255ac0cda50879ec492
ximeriverad <al261261@edu.uaa.mx>
2025-09-23 16:15:34 -0600

    Parcial02 - Add Template Forms in Angular

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  protected creds: any = {};
 
+    login(): void {
+      console.log(this.creds);
+    }
 }
