95825a7af5ef387bc8dfbaf632602685cfe3f8bb
ximeriverad <al261261@edu.uaa.mx>
2025-09-29 13:56:58 -0600

    Parcial02 - Add Home Page

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -4,9 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
