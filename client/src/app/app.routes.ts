7fd1451136acb95b173f8cbae48205c9f625b30a
ximeriverad <al261261@edu.uaa.mx>
2025-10-10 09:31:20 -0600

    Parcial02 - Add Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetail } from '../features/members/member-detail/member-detail';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: Home },
+  { path: "members", component: MemberList },
+  { path: "members/{id}", component: MemberDetail },
+  { path: "lists", component: Lists },
+  { path: "messages", component: Messages },
+  { path: "**", component: Home }
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,17 +3,19 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
+  protected router = inject(Router);
   protected readonly title = signal('Dating App');
   protected members = signal<User[]>([]);
   async ngOnInit(): Promise<void> {
