a310fef9c970d08d039ec6056d82bc3e34bb556d
ximeriverad <al261261@edu.uaa.mx>
2025-10-12 18:52:34 -0600

    Parcial02 - Add routes to guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetail } from '../features/members/member-detail/member-detail';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
-
+import { authGuard } from '../core/guard/auth-guard';
 
 export const routes: Routes = [
   { path: "", component: Home },
-  { path: "members", component: MemberList },
-  { path: "members/{id}", component: MemberDetail },
-  { path: "lists", component: Lists },
-  { path: "messages", component: Messages },
+  {
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [authGuard],
+    children: [
+      { path: "members", component: MemberList },
+      { path: "members/{id}", component: MemberDetail },
+      { path: "lists", component: Lists },
+      { path: "messages", component: Messages }
+    ]
+  },
   { path: "**", component: Home }
 ];
