9e6c7cb10b102e1d0dfd41ae02135c60207b4540
ximeriverad <al261261@edu.uaa.mx>
2025-10-14 13:09:49 -0600

    Parcial02 - Add Http Interceptor

---
+++ client/src/core/interceptors/error-interceptor.ts
@@ -0,0 +1,35 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { ToastService } from '../services/toast-service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toast = inject(ToastService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError(error => {
+      if (error) {
+        switch (error.status) {
+          case 400:
+            toast.error(error.error);
+            break;
+          case 401:
+            toast.error("Unauthorized");
+            break;
+          case 404:
+            toast.error("Not found");
+            break;
+          case 500:
+            toast.error("Server error");
+            break;
+          default:
+            toast.error("The unexpected happened!")
+            break;
+        }
+      }
+      throw error;
+    })
+  );
+};
