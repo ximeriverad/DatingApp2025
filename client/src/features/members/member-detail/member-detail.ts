e613fab8d5cd71b16709470451aa5b815c7e3e35
ximeriverad <al261261@edu.uaa.mx>
2025-11-30 16:58:37 -0600

    Parcial03 - Add custom AGE pipe

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Member } from '../../../types/member';
 import { filter } from 'rxjs';
+import { AgePipe } from '../../../core/pipes/age-pipe';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
