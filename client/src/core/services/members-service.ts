d459cd90826846b0f58e30d05a057166ba311cff
ximeriverad <al261261@edu.uaa.mx>
2025-11-30 16:48:05 -0600

    Parcial03 - Add Photos content

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,5 +17,9 @@ export class MembersService {
 
   getMembers(): Observable<Member[]> {
       return this.http.get<Member[]>(this.baseUrl + "members");
+  }
+
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
   }
 }
--- client/src/features/member-photos/member-photos.ts
+++ client/src/features/member-photos/member-photos.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MembersService } from '../../core/services/members-service';
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../../types/member';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos {
+private membersService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  protected photos$?: Observable<Photo[]>;
 
+  constructor() {
+    const memberId = this.route.parent?.snapshot.paramMap.get("id");
+    if (memberId) {
+      this.photos$ = this.membersService.getPhotos(memberId);
+    }
+  }
+
+  get photoMocks() {
+    return Array.from({ length: 20 }, (_, i) => ({
+      url: "./user.jpg"
+    }));
+  }
 }
