ba4f152e8b6198a69a232ae8fe5669c32fdec742
ximeriverad <al261261@edu.uaa.mx>
2025-11-30 16:43:22 -0600

    Parcial03 - Add Profile content

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,18 +1,15 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MembersService } from '../../../core/services/members-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../types/member';
-import { filter, Observable } from 'rxjs';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
 export class MemberDetail implements OnInit {
-  private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   protected member = signal<Member | undefined>(undefined);
