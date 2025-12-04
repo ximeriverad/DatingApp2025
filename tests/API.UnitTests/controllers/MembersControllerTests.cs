bf3857074d9306cdb4874987bba90f05e02d75eb
ximeriverad <al261261@edu.uaa.mx>
2025-12-04 12:56:31 -0600

    Parcial03 - Update MembersController tests

--- tests/API.UnitTests/controllers/MembersControllerTests.cs
+++ tests/API.UnitTests/controllers/MembersControllerTests.cs
@@ -14,17 +14,21 @@ public class MembersControllerTests
     private MembersController _membersController;
     private IMembersRepository _mockMembersRepository;
 
+    private const int NOT_FOUND = 404;
+    private const string NOT_FOUND_ERROR_MESSAGE = "Expected NotFoundObjectResult but got something else";
+    private const string OK_ERROR_MESSAGE = "Expected OkObjectResult but got something else";
+
     [SetUp]
     public void Setup()
     {
         _mockMembersRepository = Substitute.For<IMembersRepository>();
         _membersController = new MembersController(_mockMembersRepository);
-    }
 
-    [Test]
-    public async Task GetMembers_Valid_ReturnMembers()
-    {
-        // Arrange
+
+
+
+
+
         var userId = "userId";
         DefaultHttpContext testHttpContext = new()
         {
@@ -36,32 +40,19 @@ public class MembersControllerTests
         {
             HttpContext = testHttpContext
         };
+    }
 
-        IReadOnlyList<Member> expectedMembers =
-        [
-            new Member
-            {
-                Id = "test-id",
-                BirthDay = DateOnly.Parse("2000-01-01"),
-                ImageUrl = null,
-                DisplayName = "Test",
-                Created = DateTime.UtcNow,
-                LastActive = DateTime.UtcNow,
-                Gender = "Gender",
-                Description = "Description",
-                City = "City",
-                Country = "Country",
-                User = null!,
-                Photos = []
-            }
-        ];
-
+    [Test]
+    public async Task GetMembers_Valid_ReturnMembers()
+    {
+        // Arrange
+        IReadOnlyList<Member> expectedMembers = [GetTestMember() ];
         _mockMembersRepository.GetMembersAsync().Returns(expectedMembers);
 
         // Act & Assert
         var membersResult = await _membersController.GetMembers();
         var okResult = membersResult.Result as OkObjectResult;
-        Assert.That(okResult, Is.Not.Null, "Expected OkObjectResult but got something else");
+        Assert.That(okResult, Is.Not.Null, OK_ERROR_MESSAGE);
 
         var members = okResult.Value as IReadOnlyList<Member>;
         Assert.That(members, Is.Not.Null);
@@ -75,34 +66,7 @@ public class MembersControllerTests
     public async Task GetMember_Valid_ReturnMembers()
     {
         // Arrange
-        var userId = "userId";
-        DefaultHttpContext testHttpContext = new()
-        {
-            User = new ClaimsPrincipal(new ClaimsIdentity([
-                new Claim("email", userId)
-            ]))
-        };
-        _membersController.ControllerContext = new ControllerContext
-        {
-            HttpContext = testHttpContext
-        };
-
-        Member expectedMember = new()
-        {
-            Id = "test-id",
-            BirthDay = DateOnly.Parse("2000-01-01"),
-            ImageUrl = null,
-            DisplayName = "Test",
-            Created = DateTime.UtcNow,
-            LastActive = DateTime.UtcNow,
-            Gender = "Gender",
-            Description = "Description",
-            City = "City",
-            Country = "Country",
-            User = null!,
-            Photos = []
-        };
-
+        Member expectedMember = GetTestMember();
         _mockMembersRepository.GetMemberAsync(expectedMember.Id).Returns(expectedMember);
 
         // Act
@@ -123,19 +87,45 @@ public class MembersControllerTests
     public async Task GetMember_Valid_ReturnNotFound()
     {
         // Arrange
-        var userId = "userId";
-        DefaultHttpContext testHttpContext = new()
-        {
-            User = new ClaimsPrincipal(new ClaimsIdentity([
-                new Claim("email", userId)
-            ]))
-        };
-        _membersController.ControllerContext = new ControllerContext
-        {
-            HttpContext = testHttpContext
-        };
+        Member expectedMember = GetTestMember();
 
-        Member expectedMember = new()
+        _mockMembersRepository.GetMemberAsync(expectedMember.Id).ReturnsNull();
+
+        // Act & Assert
+        var memberResult = await _membersController.GetMember(expectedMember.Id);
+        var notFoundResult = memberResult.Result as NotFoundResult;
+        Assert.That(notFoundResult, Is.Not.Null, NOT_FOUND_ERROR_MESSAGE);
+        Assert.That(notFoundResult.StatusCode, Is.EqualTo(NOT_FOUND), NOT_FOUND_ERROR_MESSAGE);
+
+        var member = memberResult.Value;
+        Assert.That(member, Is.Null);
+    }
+
+    [Test]
+    public async Task GetPhotos_Valid_ReturnPhotos()
+    {
+        // Arrange
+        var expectedMember = GetTestMember();
+        IReadOnlyList<Photo> expectedPhotos = [GetTestPhoto() ];
+
+        _mockMembersRepository.GetPhotosAsync(expectedMember.Id).Returns(expectedPhotos);
+
+        // Act & Assert
+        var photosResult = await _membersController.GetPhotos(expectedMember.Id);
+        var okResult = photosResult.Result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null, OK_ERROR_MESSAGE);
+
+        var photos = okResult.Value as IReadOnlyList<Photo>;
+        Assert.That(photos, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(photos, Has.Count.EqualTo(1));
+        });
+    }
+
+    private static Member GetTestMember()
+    {
+        return new Member
         {
             Id = "test-id",
             BirthDay = DateOnly.Parse("2000-01-01"),
@@ -150,15 +140,17 @@ public class MembersControllerTests
             User = null!,
             Photos = []
         };
+    }
 
-        _mockMembersRepository.GetMemberAsync(expectedMember.Id).ReturnsNull();
-
-        // Act & Assert
-        var memberResult = await _membersController.GetMember(expectedMember.Id);
-        var notFoundResult = memberResult.Result as NotFoundObjectResult;
-        Assert.That(notFoundResult, Is.Not.Null, "Expected NotFoundObjectResult but got something else");
-
-        var member = memberResult.Value;
-        Assert.That(member, Is.Null);
+    private static Photo GetTestPhoto()
+    {
+        return new Photo()
+        {
+            Id = 1,
+            Url = "Url",
+            PublicId = "",
+            Member = GetTestMember(),
+            MemberId = GetTestMember().Id
+        };
     }
 }
